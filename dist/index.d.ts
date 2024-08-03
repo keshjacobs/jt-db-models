a523704aff55389f06452540c9bd2de5acc6a988
Abraham <brhmdeen@gmail.com>
2024-08-03 04:49:32 +0100

    default exports

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,2 +1,2 @@
-export * from './models/userModel';
-export * from './models/castModel';
+export { default as User } from './models/userModel';
+export { default as Cast } from './models/castModel';
--- dist/index.js
+++ dist/index.js
@@ -1,18 +1,10 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
-    }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __exportStar = (this && this.__exportStar) || function(m, exports) {
-    for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-__exportStar(require("./models/userModel"), exports);
-__exportStar(require("./models/castModel"), exports);
+exports.Cast = exports.User = void 0;
+var userModel_1 = require("./models/userModel");
+Object.defineProperty(exports, "User", { enumerable: true, get: function () { return __importDefault(userModel_1).default; } });
+var castModel_1 = require("./models/castModel");
+Object.defineProperty(exports, "Cast", { enumerable: true, get: function () { return __importDefault(castModel_1).default; } });
